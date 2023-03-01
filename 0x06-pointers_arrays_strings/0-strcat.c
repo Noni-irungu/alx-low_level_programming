1b88bbe71a6b5a87f06fbfca3d946fa9ca716410
Noni-irungu <irungudiana1@gmail.com>
2023-02-28 23:15:37 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: input value (s1)
+ * @src: input value (s2)
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int j = 0, k = 0, d = 0;
+
+	while (dest[i] != '\0')
+	{
+		j++;
+	}
+	while (src[k] != '\0')
+	{
+		k++;
+	}
+	while (d <= c)
+	{
+		dest[j] = src[n];
+		d++;
+		j++;
+	}
+	return (dest);
+}
