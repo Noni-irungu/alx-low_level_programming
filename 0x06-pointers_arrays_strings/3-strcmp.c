931c8f2473f5da284709b623a88385897afd08ff
Noni-irungu <irungudiana1@gmail.com>
2023-03-01 13:33:02 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int k;
-
-	while ((s1[k] == s2[k]) && (s1[k] != '\0'))
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		k++;
+	s1++;
+	s2++;
 	}
-	return (s1[k] - s2[k]);
+	return (*s1 - *s2);
 }
