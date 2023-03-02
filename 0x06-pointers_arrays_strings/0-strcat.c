7dbb8dc974a3d727ff6139c3118ef11ae2a8429b
Noni-irungu <irungudiana1@gmail.com>
2023-03-02 07:54:09 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,24 +2,21 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: The destination string
- * @src: The source string
+ * @dest: A pointer to the string pointed to by @src, including the x
+ * null byte, to the end of the string pointed to by @dest.
+ * @src: The source string to be appended to @dest
  *
- * Return: A pointer to the resulting string dest
+ * Return: A pointer to the destination string @dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i;
+	int index = 0, dest_len = 0;
 
-	while (dest[dlen])
-	{
-		dlen++;
-	}
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dlen] = src[i];
-		dlen++;
-	}
-	dest[dle] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
