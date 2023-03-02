1a9e75bddda0534f69bcd2d142d0699c250822b6
Noni-irungu <irungudiana1@gmail.com>
2023-03-02 08:44:13 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,31 @@
  */
 char *cap_string(char *str)
 {
-	int k = 0;
+	int index = 0;
 
-	while (s[k] != '\0')
+	while (str[index])
 	{
-		if (s[0] <= 122 && s[0] >= 97)
-		{
-			s[0] = s[0] - 32;
-		}
-		if (s[k] == 32 || s[k] == 46 || s[k] == '\t' ||
-			s[k] == '\n' || s[k] == 44 || s[k] == 59 ||
-				s[k] == '!' || s[k] == '?' || s[k] == '(' ||
-				s[k] == ')' || s[k] == '{' || s[k] == '}')
-		{
-			if (s[k + 1] <= 122 && s[k + 1] >= 97)
-			{
-				s[k + 1] = s[k + 1] - 32;
-			}
-		}
-	k++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == '' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == "" ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			str[index] = 32;
+
+		index++;
 	}
-	return (s);
+
+	return (str);
 }
