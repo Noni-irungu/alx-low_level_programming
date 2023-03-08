c626c13b0bbc0e49cfc969eb7787b2ef56fcab8a
Noni-irungu <irungudiana1@gmail.com>
2023-03-08 02:04:11 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,21 @@
 # include "main.h"
 
 /**
- * puts_recursion - a function that prints a string
+ * _puts_recursion - a function that prints a string
  * followed by a new line
  * @s: input
  *
  * Return: Always 0
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(*s++);
+		_puts_recursion(s);
 	}
-
 	else
-		_putchar('\n');
+	{
+		_putchar(10);
+	}
 }
