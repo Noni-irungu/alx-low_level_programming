2249d0670d7cc7c8ab884feaec2d1555c4022526
Noni-irungu <irungudiana1@gmail.com>
2023-03-13 16:06:42 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,21 +14,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr = NULL;
-
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	arr = malloc(size * sizeof(char));
+	char *arr = (char *) malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
