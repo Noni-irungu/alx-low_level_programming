9681c52f729389a306bf25532b6b706187002025
Noni-irungu <irungudiana1@gmail.com>
2023-03-16 13:00:39 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int g;
+
+	for (g = 0; g < n; g++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
 /**
  * _calloc - a function that allocates memory for an array
  * @nmemb: number of elements in array
@@ -12,23 +27,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int ar_size, i;
-	char *h;
+	void *z;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ar_size = nmemb * size;
 
-	h = malloc(ar_size);
+	h = malloc(nmemb * size);
 
-	if (ar == NULL)
+	if (h == NULL)
 		return (NULL);
-
-	while (i < ar_size)
-	{
-		ar[i] = 0;
-		i++;
-	}
-
-	return (ar);
+	_memset(h, 0, (nmemb * size));
+	return (h);
 }
