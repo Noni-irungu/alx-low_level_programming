b28302bd99771f094011448c101c372062057a5f
Noni-irungu <irungudiana1@gmail.com>
2023-05-04 14:44:16 -0700

    1-print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int w, cnt = 0;
-	unsigned long int curnt;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (w = 63; w >= 0; w--)
-	{
-		curnt = n >> w;
-
-		if (curnt & 1)
-		{
-			_putchar('1');
-			cnt++;
-		}
-		else if (cnt)
-			_putchar('0);
-	}
-	if (!cnt)
-		_putchar('0');
+	_putchar((n & 1) + '0');
 }
