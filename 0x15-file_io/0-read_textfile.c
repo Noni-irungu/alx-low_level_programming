cccadbb50b9e6155b8f717225cc94bf7423e0e73
Noni-irungu <irungudiana1@gmail.com>
2023-05-08 12:22:40 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,29 +6,25 @@
  * and prints it to standard output.
  * @filename: text file that's being read
  * @letters: No. of letters to read
- * Return: o/w - actual No. of bytes read and printed
+ * Return: w - actual No. of bytes read and printed
  * (0) when function fails or filename is NULL.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buff;
-	ssize_t o, r, w;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
 	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
-		return (0);
+	t = read(fd, buff, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buff, letters);
-	w = write(STDOUT_FILENO, buff, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buff);
-		close(o);
-		return (w);
+	free(buff);
+	close(o);
+	return (w);
 }
