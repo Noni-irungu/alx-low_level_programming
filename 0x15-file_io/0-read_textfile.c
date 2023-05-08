4e7fd8ed52c54b3dfdbf3273a76a87827a9b14c7
Noni-irungu <irungudiana1@gmail.com>
2023-05-08 12:40:36 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,27 +4,44 @@
 /**
  * read_textfile - the function that reads a text file
  * and prints it to standard output.
- * @filename: text file that's being read
- * @letters: No. of letters to read
- * Return: w - actual No. of bytes read and printed
- * (0) when function fails or filename is NULL.
- */
-
+ * @filename: is the text file thats is being read
+ * @letters: number of letters to read and print from file
+ *
+ * Return: 0 if it fails or actual number of letters it could
+ *         read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int doss;
+	ssize_t read_chec, worcnt;
 	char *buff;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (filename == NULL)
 		return (0);
+
+	doss = open(filename, O_RDONLY);
+
+	if (doss == -1)
+		return (0);
+
 	buff = malloc(sizeof(char) * letters);
-	t = read(fd, buff, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	if (buff == NULL)
+	{
+		free(buff);
+		return (0);
+	}
+
+	read_chec = read(doss, buff, letters);
+	if (read_chec == -1)
+		return (0);
+
+	worcnt = write(STDOUT_FILENO, buff, read_chec);
+	if (worcnt == -1 || read_chec != worcnt)
+		return (0);
 
 	free(buff);
-	close(o);
-	return (w);
+
+	close(doss);
+
+	return (worcnt);
 }
