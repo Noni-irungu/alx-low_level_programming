e49d5b06cbe2482da89b62726cc0e096a05cc14b
Noni-irungu <irungudiana1@gmail.com>
2023-05-08 11:32:24 -0700

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,25 +6,25 @@
  * and prints it to the POSIX standard output.
  * @filename: text file that's being read
  * @letters: No. of letters to read
- * Return: bytes_read - actual No. of bytes read and printed
+ * Return: w - actual No. of bytes read and printed
  * (0)failure or filename is NULL.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t total;
+	ssize_t t;
 	char *buffer;
-	ssize_t bytes_read;
+	ssize_t w;
 	ssize_t fd;
 
 	fd = open(filenmame, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	total = read(fd, buffer, letters);
-	bytes_read = write(STDOUT_FILENO, buffer, total);
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
 
 	free(buffer);
 	close(fd);
-	return (bytes_read);
+	return (w);
 }
