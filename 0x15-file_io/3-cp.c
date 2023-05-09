c5ba2f9f0868c65fb1a2b4640783156fc881fb13
Noni-irungu <irungudiana1@gmail.com>
2023-05-09 06:45:56 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -57,7 +57,7 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int mid, via, r, w;
+	int from, to, r, w;
 	char *buffer;
 
 	if (arc != 3)
@@ -67,12 +67,12 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	mid = open(argv[1], O_RDONLY);
-	r = read(mid, buff, 1024);
-	via = open(arv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buff, 1024);
+	to = open(arv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (mid == -1 || r == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -80,8 +80,8 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		w = write(via, buffer, r);
-		if (via == -1 || w == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -89,12 +89,12 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		r = read(mid, buffer, 1024);
-		via = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	free(buffer);
-	close_file(mid);
-	close_file(via);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
