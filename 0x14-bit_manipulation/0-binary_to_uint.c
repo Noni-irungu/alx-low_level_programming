623c9a7d75493d4eb52021ba11dc73d0b4bc21ca
Noni-irungu <irungudiana1@gmail.com>
2023-08-31 02:35:33 -0700

    binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,15 +17,14 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
-		value = value << 1;
-		value += b[i] - '0';
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		value = value * 2 + (b[i] - '0');
 		i++;
 	}
-
-	if (b[i] != '\0')
-		return (0);
 
 	return (value);
 }
