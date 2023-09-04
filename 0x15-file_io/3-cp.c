0d75ed6629c077392154d2ac5f6b4cf69ecaab56
Noni-irungu <irungudiana1@gmail.com>
2023-09-04 05:42:26 -0700

    Copying a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,59 +4,9 @@
 
 #define BUFF_SIZE 1024
 
-/**
- * main - a function that copies contents of a file to another file.
- * @argc: number of arguments passed to the program
- * @argv: array of pointers to the arguments
- *
- * Return: 0 on success
- *
- * Description: if the argument count is incorrect - exit code 97.
- * if file_from doesn't exist or can't be read - exit code 98.
- * if file_to can't be created or written to - exit code 99.
- * if file_to or file_from can't be closed - exit code 100.
- */
-
-int main(int argc, char **argv)
-{
-	int file0, file1, result0, result1;
-
-	char *buff;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	buff = malloc(sizeof(char) * BUFF_SIZE);
-	if (buff == NULL)
-		return (0);
-
-	file1 = open(argv[1], O_WRONLY);
-	e_98(file1, buff, argv[1]);
-	file0 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	e_99(file0, buff, argv[2]);
-
-	do {
-		result0 = read(file1, buff, BUFF_SIZE);
-
-		if (result0 == 0)
-			break;
-		e_98(result0, buff, argv[1]);
-
-		result1 = write(file0, buff, result0);
-		e_99(result1, buff, argv[2]);
-	}
-
-	while (result1 >= BUFF_SIZE);
-	result0 = close(file0);
-	e_100(result0, buff);
-	result0 = close(file1);
-	e_100(result0, buff);
-	free(buff);
-	return (0);
-}
+void e_98(int file0, char *buff, char *argv);
+void e_99(int file0, char *buff, char *argv);
+void e_100(int file0, char *buff);
 
 /**
  * e_98 - checks for the error 98.
@@ -103,4 +53,58 @@ void e_100(int file0, char *buff)
 		free(buff);
 		exit(100);
 	}
+}
+
+/**
+ * main - a function that copies contents of a file to another file.
+ * @argc: number of arguments passed to the program
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ *
+ * Description: if the argument count is incorrect - exit code 97.
+ * if file_from doesn't exist or can't be read - exit code 98.
+ * if file_to can't be created or written to - exit code 99.
+ * if file_to or file_from can't be closed - exit code 100.
+ */
+
+int main(int argc, char **argv)
+{
+	int file0, file1, result0, result1;
+
+	char *buff;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buff = malloc(sizeof(char) * BUFF_SIZE);
+	if (!buff)
+		return (0);
+
+	file1 = open(argv[1], O_WRONLY);
+	e_98(file1, buff, argv[1]);
+	file0 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	e_99(file0, buff, argv[2]);
+
+	do {
+		result0 = read(file1, buff, BUFF_SIZE);
+
+		if (result0 == 0)
+			break;
+		e_98(result0, buff, argv[1]);
+		result1 = write(file0, buff, result0);
+		e_99(result1, buff, argv[2]);
+	}
+
+	while (result1 >= BUFF_SIZE);
+	result0 = close(file0);
+	e_100(result0, buff);
+	result0 = close(file1);
+	e_100(result0, buff);
+
+	free(buff);
+	return (0);
 }
