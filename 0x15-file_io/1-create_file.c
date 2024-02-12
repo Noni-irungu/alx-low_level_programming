772c90eb1a452d010a688d2c342bab5294e74959
Noni-irungu <irungudiana1@gmail.com>
2024-02-12 05:23:06 -0800

    file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,31 +4,28 @@
  * create_file - a function that creates a file.
  * @filename: a pointer to the name of the file to be created.
  * @text_content: a pointer to a string to write to the file.
- * Return: -1 if filename is NULL and on failure, otherwise (1) on success.
+ * Return: (-1) if function fails, otherwise (1).
  *
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int cf, temp;
+	int cf, k, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	cf = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (cf == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		temp = write(cf, text_content, strlen(text_content));
-
-		if (temp == -1)
-		{
-			close(cf);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
+
+	cf = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	k = write(cf, text_content, len);
+
+	if (cf == -1 || k == -1)
+		return (-1);
 
 	close(cf);
 
