993ff1a3534e73fd99de1b9622bb0655ba6753b3
Noni-irungu <irungudiana1@gmail.com>
2024-02-12 04:52:05 -0800

    file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,7 @@
  * @filename: a pointer to the name of the file to be created.
  * @text_content: a pointer to a string to write to the file.
  * Return: -1 if filename is NULL and on failure, otherwise (1) on success.
+ *
  */
 
 int create_file(const char *filename, char *text_content)
@@ -20,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	cf = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	cf = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	temp = write(cf, text_content, length);
 
 
