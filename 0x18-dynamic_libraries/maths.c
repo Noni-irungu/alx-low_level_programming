d0ee2f9d9268d34f056926d786a6e7b06d29c378
Noni-irungu <irungudiana1@gmail.com>
2024-03-25 13:35:45 -0700

    Calling C functions from python

---
+++ 0x18-dynamic_libraries/maths.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - a function that adds two integers.
+ * @num_1: first integer.
+ * @num_2: second integer.
+ *
+ * Return: addition.
+ */
+int add(int num_1, int num_2)
+{
+	return (num_1 + num_2);
+}
+
+/**
+ * sub - a function that subtracts two integers.
+ * @num_1: first integer.
+ * @num_2: second integer.
+ *
+ * Return: subtraction.
+ */
+int sub(int num_1, int num_2)
+{
+	return (num_1 - num_2);
+}
+
+/**
+ * mul - a function that multiplies two integers.
+ * @num_1: first integer.
+ * @num_2: second integer.
+ *
+ * Return: multiplication.
+ */
+int mul(int num_1, int num_2)
+{
+	return (num_1 * num_2);
+}
+
+/**
+ * div - a function that divides two integers.
+ * @num_1: first integer.
+ * @num_2: second integer.
+ *
+ * Return: multiplication.
+ */
+int div(int num_1, int num_2)
+{
+	return (num_1 / num_2);
+}
+
+/**
+ * mod - a function that gives the remainder of two integers.
+ * @num_1: first integer.
+ * @num_2: second integer.
+ *
+ * Return: final answer.
+ */
+int mod(int num_1, int num_2)
+{
+	return (num_1 % num_2);
+}
